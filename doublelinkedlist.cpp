a64c8de42a36b37a1f682ab06f717aae021d8e76
Reyanii <indriyaniv9@gmail.com>
2025-05-15 11:33:28 +0800

    step 1

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -81,4 +81,29 @@ class DoubleLinledList
 
      current->next = newNode;
     }
+
+    void hapus ()
+    { 
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+        
+        cout << "\nEnter the roll number of the student whose record is to be deleted";
+        int rollNo;
+        cin >> rollNo;
+
+        Node *current = START;
+
+        // step 1 Traverse the list to find the node
+        while (current != NULL && current->noMhs != rollNo)
+            current = current->next;
+
+        if (current == NULL)
+        {
+            cout << "Record not found" << endl;
+            return;
+        }
+    }
 }
