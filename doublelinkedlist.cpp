59716bd6aec2e8b43554f78195bd85efc56b388a
Reyanii <indriyaniv9@gmail.com>
2025-05-15 11:40:28 +0800

    step 4

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -115,7 +115,14 @@ class DoubleLinledList
         }
         else
         {
-            
+            // step 3 link previous node to next of current
+            current->prev->next = current->next;
+
+            //step 4 if current is not the last node
+            if (current->next != NULL)
+                current->next->prev = current->prev;
         }
+
+        
     }
 }
